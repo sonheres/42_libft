773d18861b736d543fb4930c5865389cb10b56db
sohernan <sonheres@gmail.com>
2023-09-19 18:05:40 +0200

    WIP

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sonheres <sonheres@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 12:29:52 by sonheres          #+#    #+#             */
+/*   Updated: 2023/09/19 17:52:27 by sonheres         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include    <unistd.h>
+
+void    ft_print_alphabet(void)
+{
+    char letra;
+    
+    letra = 'a';
+
+while (letra < 'z')
+    {
+        write (1, &letra, 1);
+        letra ++;
+    }
+}
+ 
+ int main(void)
+{
+    ft_print_alphabet();
+ }
+
