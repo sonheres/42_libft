a66269606dbdd6678d7c117d164ac7043f61e767
sohernan <sonheres@gmail.com>
2023-10-20 17:46:22 +0200

    20_10_23

---
+++ Libft/ft_atoi.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sonheres <sonheres@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/19 15:06:53 by sonheres          #+#    #+#             */
+/*   Updated: 2023/10/20 17:46:01 by sonheres         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+// CONVIERTE UNA CADENA ASCII en 'int'. Permite realizar operaciones con los 'int'.
+
+int ft_atoi(const char *str)
+{
+	int i;
+	int sign;
+	int result;
+
+	i = 0;
+	sign = 1;									//??
+	result = 0; //no es un contador. Se acumulan los dígitos aunque parte de 0
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ' || str[i] == '\t')
+		{
+			i++;
+		}
+		else if (str[i] == '-')
+		{
+			sign = -1;
+			i++;
+		}
+		else if (str[i] == '+')
+		{
+			sign = 1;
+			i++;
+		}					
+		else
+		{
+			while (str[i] >= '0' && str[i] <= '9')
+			{
+				result = result * 10;
+				result = result + str[i] - '0';
+				i++;
+			}
+			return (result * sign);		// que devuelva el resultado en el bucle de los números. Mientras haya números, que siga.
+		}
+		
+	}		
+	return (result * sign);
+}
+
+int main(void)
+{
+	//const char str[] = "  5842";
+	//const char  str[] = "asrd";
+	//const char str[] = "   8+5";
+	const char str[] = "   0";
+	//const char  str[] = " -12";
+
+	printf("original = %i\n", atoi(str));
+	printf("my = %i\n", ft_atoi(str));
+	return (0);
+}
