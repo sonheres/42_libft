eda199ceabb3563fe282aadf388adda8e368a295
sohernan <sonheres@gmail.com>
2023-11-14 17:46:52 +0100

    14_11_23_ limpieza

--- Libft/main.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-int	main(void)
-{
-	char	cad_src[] = "puri";
-
-    printf("original: %i\n", strchr(cad_src, '\0') - cad_src);
-    printf("otra    : %i\n", ft_strchr(cad_src, '\0') );
-}
